43fbcc2693120e8fab6fb4b5281b6b185d1f0b31
park-david <davidpark1094@gmail.com>
2023-06-05 22:04:10 -0700

    setup buttons for different pages, setup grid

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 const pages = document.querySelectorAll('section')
 const pageButton = document.querySelectorAll('button')
 
+for (let i = 0; i < pages.length; i++) {
+    const btn = pageButton[i]
+    const page = pages[i]
+
+    btn.addEventListener('click', (event) => {
+        for(let section of pages) {
+            if(event.target.innerText.toLowerCase() === section.id) {
+                section.style.display = 'block'
+            } else {
+                section.style.display = 'none'
+            }
+        }
+    })
+    if (page.id === 'home') {
+        page.style.display = 'block'
+    } else {
+        page.style.display = 'none'
+    }
+}
+
+const openBtn = document.getElementById('modal')
