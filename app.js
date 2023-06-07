5733fa9be9126aaa58bfc1870e8729a838c4016b
park-david <davidpark1094@gmail.com>
2023-06-06 22:43:52 -0700

    added carousel, modal

--- app.js
+++ app.js
@@ -21,4 +21,51 @@ for (let i = 0; i < pages.length; i++) {
     }
 }
 
-const openBtn = document.getElementById('modal')
+// contact me modal
+const modal = document.getElementById('modal')
+const modalBtn = document.getElementById('modal-open')
+const close = document.getElementById('modal-close')
+
+
+modal.style.display = 'none'
+
+const openModal = () => {
+    modal.style.display = 'block'
+}
+const closeModal = () => {
+    modal.style.display = 'none'
+}
+
+modalBtn.addEventListener('click', openModal)
+close.addEventListener('click', closeModal)
+
+
+// about me carousel
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+const images = document.getElementsByClassName('images');
+
+let currentImg = 0;
+let previousImg = 0;
+
+next.addEventListener('click', () => {
+    previousImg = currentImg
+    currentImg++
+
+    if (currentImg >= images.length) {
+        currentImg = 0;
+    }
+    images[previousImg].style.display = 'none'
+    images[currentImg].style.display = 'block'
+})
+
+prev.addEventListener('click', () => {
+    previousImg = currentImg
+    currentImg--
+
+    if (currentImg < 0) {
+        currentImg = images.length - 1;
+    }
+    images[previousImg].style.display = 'none'
+    images[currentImg].style.display = 'block'
+})
